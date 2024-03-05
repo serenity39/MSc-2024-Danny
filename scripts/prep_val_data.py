f9058e7372ec4cee83fd22324e842444a79022ae
Serenity39 <69771022+serenity39@users.noreply.github.com>
2024-03-05 18:37:28 +0100

    fix: logging

--- scripts/prep_val_data.py
+++ scripts/prep_val_data.py
@@ -27,17 +27,26 @@ def create_validation_set(dataset, output_csv_path):
     qid_to_text = {}
     docid_to_text = {}
 
+    spinner = Halo(
+        text="Populating dictionaries with queries and doc text...",
+        spinner="dots",
+    )
+    spinner.start()
     # Populate the dictionary with the queries and documents
     for query in dataset.queries_iter():
         qid_to_text[query.query_id] = query.text
     for doc in dataset.docs_iter():
         docid_to_text[doc.doc_id] = doc.text
+    spinner.succeed("Dictionaries populated!")
 
+    spinner.start("Prepare the validation set tuples...")
     # Prepare the validation set tuples
     validation_set = []
     for qrel in dataset.qrels_iter():
         validation_set.append((qrel.query_id, qrel.doc_id, qrel.relevance))
+    spinner.succeed("Validation set tuples prepared!")
 
+    spinner.start("Write validation set to a CSV file...")
     # Write validation set to a CSV file
     with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
         csv_writer = csv.writer(file)
@@ -45,19 +54,23 @@ def create_validation_set(dataset, output_csv_path):
             query_text = qid_to_text.get(query_id, "")
             doc_text = docid_to_text.get(doc_id, "")
             csv_writer.writerow([query_text, doc_text, relevance])
+    spinner.succeed("Validation set written to CSV file!")
 
 
 if __name__ == "__main__":
+    spinner = Halo(text="Load dataset...", spinner="dots")
+    spinner.start()
     # Load the MS MARCO dataset for validation
     dataset_validation = ir_datasets.load(
         "msmarco-passage-v2/trec-dl-2021/judged"
     )
+    spinner.succeed("Dataset loaded!")
 
     # Define path to save the validation data
     validation_csv_path = "../data/validationdata/validation_data.csv"
     os.makedirs(validation_csv_path, exist_ok=True)
 
-    spinner = Halo(text="Creating validation set...", spinner="dots")
+    spinner.start("Creating validation set...")
     # Create the validation set and save to CSV
     create_validation_set(dataset_validation, validation_csv_path)
     spinner.succeed("Validation set created!")
