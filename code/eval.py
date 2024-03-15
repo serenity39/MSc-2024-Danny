cc509c1b34b87f1cd87f24ffda0f6b10783494c7
Serenity39 <69771022+serenity39@users.noreply.github.com>
2024-03-15 15:20:44 +0100

    check hf dataset format

--- code/eval.py
+++ code/eval.py
@@ -34,6 +34,30 @@ def show_random_elements(dataset, num_examples=10):
     print(df)
 
 
+def show_first_elements(dataset, num_examples=50):
+    """Show the first elements from a Huggingface dataset.
+
+    Args:
+        dataset: dataset to pick the examples from.
+        num_examples (optional): number of examples to show. Defaults to 50.
+    """
+    assert num_examples <= len(
+        dataset
+    ), "Can't pick more elements than there are in the dataset."
+
+    df = pd.DataFrame(dataset.select(range(num_examples)))
+    for column, typ in dataset.features.items():
+        if isinstance(typ, ClassLabel):
+            df[column] = df[column].transform(lambda i: typ.names[i])
+        elif isinstance(typ, Sequence) and isinstance(typ.feature, ClassLabel):
+            df[column] = df[column].transform(
+                lambda x: [typ.feature.names[i] for i in x]
+            )
+    print(df)
+
+
 dataset = load_from_disk("../data/hf_datasets/depth_based_50_50")
 
 show_random_elements(dataset)
+print()
+show_first_elements(dataset)
