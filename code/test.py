1fda617dd0f87e29279dca54fa2f18be2de8c460
Serenity39 <69771022+serenity39@users.noreply.github.com>
2024-03-20 18:27:36 +0100

    verify model is working

---
+++ code/test.py
@@ -0,0 +1,50 @@
+"""Test model"""
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+import torch  # noqa: E402
+from transformers import BertForSequenceClassification, BertTokenizer
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load your fine-tuned model and tokenizer
+model_path = "../data/results/models/shallow_based_5000_1/"
+tokenizer = BertTokenizer.from_pretrained(model_path)
+model = BertForSequenceClassification.from_pretrained(model_path)
+model = model.to(device)
+
+# Example text pairs
+text_pairs = [
+    (
+        "What is baron nashor?",
+        "Baron Nashor is the most powerful neutral monster of the summoners rift.",
+    ),
+    (
+        "What is baron nashor?",
+        "Baron Nashor is a powerful monster that requires multiple champions to defeat.",
+    ),
+    ("What is baron nashor?", "Monkeys likes bananas. They are yellow."),
+]
+
+for query, segment in text_pairs:
+    # Tokenize inputs
+    inputs = tokenizer.encode_plus(
+        query,
+        segment,
+        return_tensors="pt",
+        max_length=512,
+        truncation=True,
+        padding=True,
+    )
+
+    # Make predictions without calculating gradients
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=1)
+        similarity_score = probabilities[:, 1].item()
+        print(
+            f"Similarity between: '{query}' and '{segment}' is {similarity_score}"
+        )
