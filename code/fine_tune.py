27598e236625f7a29dfcfa03b0cc4078bb019be6
Serenity39 <69771022+serenity39@users.noreply.github.com>
2024-03-21 13:12:06 +0100

    run early stop with depth 50_200

--- code/fine_tune.py
+++ code/fine_tune.py
@@ -23,10 +23,10 @@ logging.basicConfig(
 )
 
 # Configs
-DATASET_PATH = "../data/hf_datasets/depth_based_50_100"
-MODEL_SAVE_PATH = "../data/results/models/early_stopping/depth_based_50_100"
+DATASET_PATH = "../data/hf_datasets/depth_based_50_200"
+MODEL_SAVE_PATH = "../data/results/models/early_stopping/depth_based_50_200"
 CHECKPOINT_PATH = (
-    "../data/results/checkpoints/early_stopping/depth_based_50_100"
+    "../data/results/checkpoints/early_stopping/depth_based_50_200"
 )
 
 
